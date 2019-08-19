6228662202f1f48d85f84f3412a69430e12fc063
Davo Davosky <davodavosky@gmail.com>
2019-08-19 19:44:40 +0200

    Configured requirements for assistance and styled errors

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -1,2 +1,6 @@
 class Assistance < ApplicationRecord
+  validates :name, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :creation_date, presence: true
 end
